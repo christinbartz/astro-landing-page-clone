3822427da83d33be1b1c7b089bc8efef11da1a44
Christin Bartz <bartz@neoskop.de>
2022-09-06 16:11:02 +0200

    add helpers

---
+++ src/components.js
@@ -0,0 +1,24 @@
+import Stage from "./components/Stage.astro";
+import Text from "./components/Text.astro";
+import Headline from "./components/Headline.astro";
+import Card from "./components/Card.astro";
+import Column from "./components/Column.astro";
+import Row from "./components/Row.astro";
+
+export const Components = {
+  Stage,
+  Text,
+  Headline,
+  Card,
+  Column,
+  Row,
+
+  //new components
+  Button,
+  Code,
+  Section,
+  TitleIcon,
+  Tabs,
+  Link,
+  CardList,
+};
