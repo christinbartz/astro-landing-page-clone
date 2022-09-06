c4c42ca9a87f475cf4ea463c14b3af9368b34cc7
Christin Bartz <bartz@neoskop.de>
2022-09-06 16:33:30 +0200

    add text component

--- src/components.js
+++ src/components.js
@@ -4,6 +4,13 @@ import Headline from "./components/Headline.astro";
 import Card from "./components/Card.astro";
 import Column from "./components/Column.astro";
 import Row from "./components/Row.astro";
+import Button from "./components/Button.astro";
+import Code from "./components/Code.astro";
+import Section from "./components/Section.astro";
+import TitleIcon from "./components/TitleIcon.astro";
+import Tabs from "./components/Tabs.astro";
+import Link from "./components/Link.astro";
+import CardList from "./components/CardList.astro";
 
 export const Components = {
   Stage,
@@ -12,8 +19,6 @@ export const Components = {
   Card,
   Column,
   Row,
-
-  //new components
   Button,
   Code,
   Section,
